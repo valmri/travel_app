bb238d96f983c66e33f823b8e2a30f3b1921c3d7
valmri <valmarmie87@outlook.com>
2024-11-20 18:50:22 +0100

    feat: connect API with Delete button

--- front/src/components/TravelCardItem.tsx
+++ front/src/components/TravelCardItem.tsx
@@ -5,49 +5,57 @@ import Typography from "./ui/Typography";
 import Modal from "./ui/Modal";
 
 type TravelCardItemProps = {
-    travel: TravelType
-    travelList: TravelType[]
-    setTravelList: (travelList: TravelType[]) => void
-}
+  travel: TravelType;
+  travelList: TravelType[];
+  setTravelList: (travelList: TravelType[]) => void;
+};
 
-const TravelCardItem = ({ travel, travelList, setTravelList } : TravelCardItemProps) => {
+const TravelCardItem = ({
+  travel,
+  travelList,
+  setTravelList,
+}: TravelCardItemProps) => {
+  const handleDelete = async () => {
+    const index = travelList.indexOf(travel);
 
-  const handleDelete = () => {
-      const index = travelList.indexOf(travel)
-      travelList.splice(index, 1)
-      setTravelList([...travelList])
-  }
+    const response = await fetch(`http://localhost:8000/travels/${travel.id}`, {
+      method: "DELETE",
+      headers: { "Content-Type": "application/json" },
+    });
 
-    return ( 
-        <div className="shadow-md rounded-md">
-          <Link to={`/travels/${travel.id}`}>
-            <img src={travel.image} alt="" className="w-full" />
-          </Link>
-          
-          <div className="p-4">
-            <Link to={`/travels/${travel.id}`}>
-              <Typography level={3}>
-                  {travel.name}
-              </Typography>
-            </Link>
-            <p>{travel?.description?.substring(10)}...</p>
-          </div>
+    if (response.ok) {
+      travelList.splice(index, 1);
+      setTravelList([...travelList]);
+    }
+  };
 
-          <Modal>
-            <div className="flex flex-col gap-4">
-              <p className="text-slate-500">
-                Êtes-vous sûr de vouloir supprimer ce voyage ?
-              </p>
-              <Button 
-                text="Confirm to delete"
-                variant="danger"
-                onClick={handleDelete}
-              />
-            </div>
-          </Modal>
+  return (
+    <div className="shadow-md rounded-md">
+      <Link to={`/travels/${travel.id}`}>
+        <img src={travel.image} alt="" className="w-full" />
+      </Link>
 
+      <div className="p-4">
+        <Link to={`/travels/${travel.id}`}>
+          <Typography level={3}>{travel.name}</Typography>
+        </Link>
+        <p>{travel?.description?.substring(10)}...</p>
+      </div>
+
+      <Modal>
+        <div className="flex flex-col gap-4">
+          <p className="text-slate-500">
+            Êtes-vous sûr de vouloir supprimer ce voyage ?
+          </p>
+          <Button
+            text="Confirm to delete"
+            variant="danger"
+            onClick={handleDelete}
+          />
         </div>
-     );
-}
- 
+      </Modal>
+    </div>
+  );
+};
+
 export default TravelCardItem;
