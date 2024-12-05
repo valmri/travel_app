9392de66558430c1e8f18b599cb7aac0d11abbe8
valmri <valmarmie87@outlook.com>
2024-12-05 14:44:49 +0100

    fix: hash password

--- api/src/user/user.service.ts
+++ api/src/user/user.service.ts
@@ -1,5 +1,6 @@
 import pool from "../config/database";
 import { IUser, IUserDTO } from "./user.type";
+import bcrypt from "bcrypt";
 
 export const findAll = async (): Promise<IUser[]> => {
   const { rows } = await pool.query("SELECT * FROM users");
@@ -11,18 +12,22 @@ export const findOne = async (id: number): Promise<IUser[]> => {
   return rows[0];
 };
 
-export const create = async (IUser: IUserDTO): Promise<IUser> => {
+export const create = async (userDTO: IUserDTO): Promise<IUser> => {
+  const hashPassword = await bcrypt.hash(userDTO.password.toString(), 10);
+
   const { rows } = await pool.query(
     "INSERT INTO users (username, password) VALUES ($1, $2) RETURNING id, username, password",
-    [IUser.username, IUser.password]
+    [userDTO.username, hashPassword]
   );
   return rows[0] as IUser;
 };
 
 export const update = async (id: number, userDTO: IUserDTO): Promise<IUser> => {
+  const hashPassword = await bcrypt.hash(userDTO.password.toString(), 10);
+
   const { rows } = await pool.query(
     "UPDATE users SET username = $1, password = $2 WHERE id = $3 RETURNING id, username, password",
-    [userDTO.username, userDTO.password, id]
+    [userDTO.username, hashPassword, id]
   );
   return rows[0] as IUser;
 };
